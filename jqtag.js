bc19fae0a3387c9a182b9aeff714bbd5fc467511
Lalit Tanwar <lalit.tanwar07@gmail.com>
2015-06-10 01:45:15 +0530

    added smart tag support

--- jqtag.js
+++ jqtag.js
@@ -4,7 +4,7 @@ utils.define('jqtag').as(function(jqtag,_instance_){
 	
 	jqtag._extend_ = function(tag,_tag_){
 		tag._jqTagConfig_ = {};
-		tag._definition_(tag,tag._jqTagConfig_);
+		tag._jqTagConfig_ = tag._definition_(tag,tag._jqTagConfig_) || tag._jqTagConfig_;
 	};
 
 	jqtag.register = function(config){
@@ -14,6 +14,17 @@ utils.define('jqtag').as(function(jqtag,_instance_){
 	jqtag._extended_ = function(tag,_tag_){
 		var parentConfig = tag.parent()._jqTagConfig_ || {};
 		tag._jqTagConfig_ = $.extend(true,parentConfig,tag._jqTagConfig_);
+		if(tag._jqTagConfig_.tagName === undefined){
+			tag._jqTagConfig_.tagName = tag.module.replace(/jqtags\./,"jq-").replace(/\./g,"-");
+		}	
 		pitana.register(tag._jqTagConfig_)
 	};
 });
+
+(function(foo) {
+	
+	foo._tag_ = function(tagName,definition){
+		return utils.define(tagName).extend('jqtag').as(definition);
+	};
+	
+})(this);
