6934f3e7edb3fb580bc01cef0592475d5a52bd18
Lalit Tanwar <lalit.tanwar07@gmail.com>
2015-06-11 01:02:07 +0530

    smarter tags

--- jqtag.js
+++ jqtag.js
@@ -1,6 +1,7 @@
-utils.define('jqtag').as(function(jqtag,_instance_){
+_define_('jqtag',function(jqtag,_jqtag_){
 	
-	utils.require(":pitana/pitana.js")
+	_require_(":pitana/pitana.js");
+	var pitana = _module_("pitana");
 	
 	jqtag._extend_ = function(tag,_tag_){
 		tag._jqTagConfig_ = {};
@@ -14,17 +15,28 @@ utils.define('jqtag').as(function(jqtag,_instance_){
 	jqtag._extended_ = function(tag,_tag_){
 		var parentConfig = tag.parent()._jqTagConfig_ || {};
 		tag._jqTagConfig_ = $.extend(true,parentConfig,tag._jqTagConfig_);
-		if(tag._jqTagConfig_.tagName === undefined){
-			tag._jqTagConfig_.tagName = tag.module.replace(/jqtags\./,"jq-").replace(/\./g,"-");
-		}	
-		pitana.register(tag._jqTagConfig_)
+		jqtag._pitana_register_(tag.module,tag._jqTagConfig_);
+	};
+	jqtag._pitana_register_ = function(moduleName,_jqTagConfig_){
+		if(_jqTagConfig_.tagName === undefined){
+			_jqTagConfig_.tagName = moduleName.replace(/jqtags\./,"jq-").replace(/\./g,"-");
+		}
+		return pitana.register(_jqTagConfig_)
 	};
 });
 
 (function(foo) {
 	
+	var jqtag = _module_("jqtag");
+	var pitana = _module_("pitana");
+	
 	foo._tag_ = function(tagName,definition){
-		return utils.define(tagName).extend('jqtag').as(definition);
+		if(utils!==undefined && typeof utils.define === 'function'){
+			return utils.define(tagName).extend('jqtag').as(definition);
+		} else {
+			var module = _define_(tagName,'jqtag',definition);
+			return jqtag._pitana_register_(tagName,module._jqTagConfig_ || module)
+		}
 	};
 	
 })(this);
