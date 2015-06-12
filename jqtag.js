67f9bba3aee0f6b32219b42f7ad3b8653b3a1c1c
Lalit Tanwar <lalit.tanwar07@gmail.com>
2015-06-12 20:20:43 +0530

    added meta keys

--- jqtag.js
+++ jqtag.js
@@ -27,6 +27,45 @@ _define_('jqtag',function(jqtag,_jqtag_){
 		}
 		return pitana.register(_jqTagConfig_)
 	};
+	
+	jqtag.META_KEYS = {};
+	
+	jqtag.key = function(e){
+		var keyCode = e;
+		if(typeof e !== 'string'){
+			keyCode = e.which || e.keyCode;
+		}
+		return jqtag.META_KEYS[keyCode] || {}; 
+	};
+	
+	jqtag.key_codes = {
+		8: "backspace", 9: "tab", 13: "return", 16: "shift", 17: "ctrl", 18: "alt", 19: "pause",
+		20: "capslock", 27: "esc", 32: "space", 33: "pageup", 34: "pagedown", 35: "end", 36: "home",
+		37: "left", 38: "up", 39: "right", 40: "down", 45: "insert", 46: "del", 
+		96: "0", 97: "1",98 : "2", 99 : "3", 100 : "4", 101 : "5", 102 : "6", 103 : "7",
+		104: "8", 105: "9", 106: "*", 107: "+", 109: "-", 110: ".", 111 : "/", 
+		112: "f1", 113: "f2", 114: "f3", 115: "f4", 116: "f5", 117: "f6", 118: "f7", 119: "f8", 
+		120: "f9", 121: "f10", 122: "f11", 123: "f12", 144: "numlock", 145: "scroll", 191: "/", 224: "meta"	
+	};
+	
+	(function(){
+		for(var i in jqtag.key_codes){
+			jqtag.META_KEYS[i] = { name : jqtag.key_codes[i], code : i}
+		}
+		//Navigation Keys
+		;[9,13,37,38,39,40].map(function(code){
+			jqtag.META_KEYS[code].isNavKey = true;
+		});
+		;[37,39].map(function(code){
+			jqtag.META_KEYS[code].isNavX = true;
+		});
+		;[38,40].map(function(code){
+			jqtag.META_KEYS[code].isNavY = true;
+		});
+		
+	})();
+	
+	
 });
 
 (function(foo) {
